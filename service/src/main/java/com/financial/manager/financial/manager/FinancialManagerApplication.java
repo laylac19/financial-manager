3edc49facb098948cd5815ad52f0865971ae89e7
layla.couto <laylamscouto@gmail.com>
2023-10-12 10:19:01 -0300

    project structure

---
+++ service/src/main/java/com/financial/manager/financial/manager/FinancialManagerApplication.java
@@ -0,0 +1,13 @@
+package com.financial.manager.financial.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagerApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/financial/manager/financial/manager/FinancialManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.manager.financial.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
